856bf5c749a6cb8bedb4f6b60e6f3c66282e18d9
Don-Up <yexudong188@gmail.com>
2025-02-21 15:40:57 +0800

    feat(event-detail): add event detail components and layout
    - Create EventSummary, EventLogistics, and EventContent components
    - Add corresponding CSS modules for styling
    - Implement responsive design for different screen sizes
    - Update EventDetail page to use new components
    - Add dummy data fetching and event display logic

--- src/app/events/[eventId]/page.tsx
+++ src/app/events/[eventId]/page.tsx
@@ -1,5 +1,31 @@
-export default function EventDetail(){
-    return <div>
-        <h1>Event Detail</h1>
-    </div>
+"use client"
+import { useParams } from "next/navigation";
+import { getEventById } from "../../../../dummy-data";
+import EventSummary from "@/components/event-detail/event-summary";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventContent from "@/components/event-detail/event-content";
+
+export default function EventDetail() {
+    const params = useParams();
+    const eventId = params.eventId as string; // Cast to string if necessary
+    const event = getEventById(eventId);
+
+    if (!event) {
+        return <p>No event found</p>;
+    }
+
+    return (
+        <>
+            <EventSummary title={event.title} />
+            <EventLogistics
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </>
+    );
 }
